f1df61130e264e12912381865fd8dcee8dc74b7c
naseem1amjad <naseem@technologist.com>
2021-10-08 12:39:13 +0500

    Uploading Source

---
+++ Assets/changeColor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    private Material selectedMaterial;
+
+    public Material[] colourableMaterials;  //put GameObject materials here
+
+    public Color[] colors = { Color.red, Color.blue, Color.green, Color.yellow };  //put colors here
+    void Start()
+    {
+
+    }
+
+    public void SelectMaterial(int i)
+    {
+        selectedMaterial = colourableMaterials[i];
+    }
+    public void SetColor(int i)
+    {
+        selectedMaterial.color = colors[i];
+    }
+
+    public void onRed()
+    {
+        SelectMaterial(0);
+        SetColor(0);
+    }
+
+    public void onBlue()
+    {
+        SelectMaterial(0);
+        SetColor(1);
+    }
+
+    public void onGreen()
+    {
+        SelectMaterial(0);
+        SetColor(2);
+    }
+
+    public void onYellow()
+    {
+        SelectMaterial(0);
+        SetColor(3);
+    }
+
+}
---
+++ Assets/rotObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotObj : MonoBehaviour
+{
+    float rotSpeed = 20;
+
+    public float xSpeed = 2f;
+    public float ySpeed = 0.0f;
+    public float zSpeed = 0.0f;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, rotY);
+
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(
+             xSpeed * Time.deltaTime,
+             ySpeed * Time.deltaTime,
+             zSpeed * Time.deltaTime
+        );
+    }
+}
